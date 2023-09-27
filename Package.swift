e9592bc5f07e089c2b6f5719b9ed1e50cde7ab76
Ajay Dhasal <ajaydhasal@gmail.com>
2023-09-27 11:18:04 +0530

    Update Package.swift
    BLEManager

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BLELibrary",
-            targets: ["BLELibrary"]),
+            targets: ["BLELibrary", "BLEManager"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
